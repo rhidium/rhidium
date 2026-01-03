da8c62b7672150b5cfde021a7329f99d49da3132
Richard Hillebrand (Mirasaki) <me@mirasaki.dev>
2026-01-04 00:24:11 +0100

    refactor(commitlint): add ignore rule for [skip ci] in commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@ const config = {
     (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
     (message) => message.startsWith('chore(release): '),
     (message) => message.startsWith('Merge commit '),
+    (message) => message.includes('[skip ci]') || message.includes('[ci skip]'),
   ],
 };
 
