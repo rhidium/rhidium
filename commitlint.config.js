62b94c1b90f233cd78bc509b86e4990ed0f951b8
Richard Hillebrand (Mirasaki) <me@mirasaki.dev>
2026-01-03 20:32:24 +0100

    refactor(commitlint): switch to ES module syntax for configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const config = {
   extends: ['@commitlint/config-conventional'],
   ignores: [
     (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
     (message) => message.startsWith('chore(release): '),
   ],
 };
+
+export default config;
