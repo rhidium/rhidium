f276a00cc2b7d95bf6b233f90057c1596f0d1c7d
Richard Hillebrand (Mirasaki) <me@mirasaki.dev>
2026-01-03 20:33:27 +0100

    refactor(prettier): switch to ES module syntax for configuration

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,10 +2,12 @@
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
-module.exports = {
+const config = {
   // ...require('gts/.prettierrc.json'),
   bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'always',
-};
+}
+
+export default config;
